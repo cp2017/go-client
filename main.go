d09e292da60162767ba47b0df748f05159111659
Christian Kniep <christian@qnib.org>
2017-01-07 10:55:45 +0100

    initial commit of go-client

---
+++ main.go
@@ -0,0 +1,47 @@
+// Copyright © 2017 Christian Kniep <christian@qnib.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+  //"fmt"
+  "os"
+
+  "github.com/cp2017/go-client/cmd"
+  "github.com/urfave/cli"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "gp-client"
+  app.Version = "0.0.1"
+
+  app.Commands = []cli.Command{
+    {
+      Name:    "monitor",
+      Aliases: []string{"m"},
+      Usage:   "Monitor a given service",
+      Flags:   []cli.Flag {
+        cli.StringFlag{
+          Name: "ipfs-id, id",
+          Usage: "HASH of service within IPFS",
+        },
+      },
+      Action:  func(c *cli.Context) error {
+        return cmd.Monitor(c)
+        },
+    },
+  }
+  app.Run(os.Args)
+}
