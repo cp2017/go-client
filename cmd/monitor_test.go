e78285c1a4b65bfb5807d815c7963dfec832faa3
Christian Kniep <christian@qnib.org>
2017-01-15 12:27:38 +0100

    fix monitor test

--- cmd/monitor_test.go
+++ cmd/monitor_test.go
@@ -1,16 +1,18 @@
 package cmd
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cp2017/go-client/util"
 )
 
 func TestValidateStatus(t *testing.T) {
-    res := map[string]int{}
-    res["200"] = 5
-    assert.True(t, ValidateStatus(res, 5, "200:100"), "5x 200, should get us a 100% 200")
-    assert.False(t, ValidateStatus(res, 10, "200:100"), "10x 200, should get us only 50% 200")
-    res["500"] = 5
-    assert.True(t, ValidateStatus(res, 10, "200:50,500:50"), "10x 200, should get us only 50% 200")
+	res := map[string]int{}
+	res["200"] = 5
+	assert.True(t, util.ValidateStatus(res, 5, "200:100"), "5x 200, should get us a 100% 200")
+	assert.False(t, util.ValidateStatus(res, 10, "200:100"), "10x 200, should get us only 50% 200")
+	res["500"] = 5
+	assert.True(t, util.ValidateStatus(res, 10, "200:50,500:50"), "10x 200, should get us only 50% 200")
 }
